aa062c671f17a48070a239a853ffd5a89b3a9298
leocassiosilva <leocassiosilva1234@gmail.com>
2021-01-29 14:20:48 -0300

    conectando a base de dados

--- fusion/settings.py
+++ fusion/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME':  'fusion',
-        'USER': 'root',
+        'USER': 'postgres',
         'PASSWORD': '123',
         'HOST': 'localhost',
         'PORT': '5432',
